4927cbe756c197a21003f48bb8d08d49280f0aac
oliverstivers <77082348+oliverstivers@users.noreply.github.com>
2024-01-06 11:17:40 -0800

    creation of robotmap

---
+++ Stealth2024/src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,33 @@
+package frc.robot;
+
+public class RobotMap {
+
+    //front left
+    public static class SwerveMod0 {
+        public static final int DRIVE_MOTOR = -1;
+        public static final int TURN_MOTOR = -1;
+        public static final int TURN_ENCODER = -1;
+    }
+
+    //front right
+    public static class SwerveMod1 {
+        public static final int DRIVE_MOTOR = -1;
+        public static final int TURN_MOTOR = -1;
+        public static final int TURN_ENCODER = -1;
+    }
+
+    //back left
+    public static class SwerveMod2 {
+        public static final int DRIVE_MOTOR = -1;
+        public static final int TURN_MOTOR = -1;
+        public static final int TURN_ENCODER = -1;
+    }
+
+    //back right
+    public static class SwerveMod3 {
+        public static final int DRIVE_MOTOR = -1;
+        public static final int TURN_MOTOR = -1;
+        public static final int TURN_ENCODER = -1;
+    }
+
+}
