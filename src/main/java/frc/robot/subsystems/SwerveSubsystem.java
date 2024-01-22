09e6cb9a5549f59aa46e1aefca8d3222bd81a9a0
oliverstivers <77082348+oliverstivers@users.noreply.github.com>
2024-01-21 19:18:11 -0800

    nothing working 💀

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -165,12 +165,12 @@ public class SwerveSubsystem extends SubsystemBase {
 
     @Override
     public void periodic() {
-        if(poseEstimationSystem.getLeftVisionEstimatePresent()){
-            addVisionMeasurement(poseEstimationSystem.getLeftVisionEstimatePose2d(), poseEstimationSystem.getLeftVisionEstimateTimestamp());
-        }
-        if(poseEstimationSystem.getRightVisionEstimatePresent()){
-            addVisionMeasurement(poseEstimationSystem.getRightVisionEstimatePose2d(), poseEstimationSystem.getRightVisionEstimateTimestamp());
-        }
+        // if(poseEstimationSystem.getLeftVisionEstimatePresent()){
+        //     addVisionMeasurement(poseEstimationSystem.getLeftVisionEstimatePose2d(), poseEstimationSystem.getLeftVisionEstimateTimestamp());
+        // }
+        // if(poseEstimationSystem.getRightVisionEstimatePresent()){
+        //     addVisionMeasurement(poseEstimationSystem.getRightVisionEstimatePose2d(), poseEstimationSystem.getRightVisionEstimateTimestamp());
+        // }
 
 
     }
