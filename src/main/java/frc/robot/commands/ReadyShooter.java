60088213d68d2fdaef836bb2ecb3a983dc0311c9
oliverstivers <77082348+oliverstivers@users.noreply.github.com>
2024-01-29 23:27:13 -0800

    added live offset to readyshooter

--- src/main/java/frc/robot/commands/ReadyShooter.java
+++ src/main/java/frc/robot/commands/ReadyShooter.java
@@ -1,5 +1,7 @@
 package frc.robot.commands;
 
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.ShooterInterpolation;
@@ -12,16 +14,27 @@ public class ReadyShooter extends Command{
     private final SwerveDrive drive;
     private final double SPIN_MULTIPLIER = 0.8;
 
-    public ReadyShooter(ShooterSubsystem shooterSubsystem, SwerveDrive drive){
+    //allows us to modify the distance the robot thinks it is from the goal in order to live offset the shooter speed/angle
+    //will use some sort of slider or other thing, either virtual on dashboard screen or a physical slider on the driverstation
+    private final DoubleSupplier distanceOffset;
+
+    public ReadyShooter(ShooterSubsystem shooterSubsystem, SwerveDrive drive, DoubleSupplier distanceOffset){
         this.shooterSubsystem = shooterSubsystem;
         this.drive = drive;
+        this.distanceOffset = distanceOffset;
         addRequirements(shooterSubsystem);
     }
+
+    public ReadyShooter(ShooterSubsystem shooterSubsystem, SwerveDrive drive){
+        this(shooterSubsystem, drive, () -> 0.0);
+    }
+
+
 
     @Override
     public void initialize() {
         // gets distance in feet because that's what the interpolation map uses
-        double distance = Units.metersToFeet(drive.getDistanceMetersToGoal());
+        double distance = Units.metersToFeet(drive.getDistanceMetersToGoal()) + distanceOffset.getAsDouble();
         double targetSpeed = ShooterInterpolation.getInterpolatedShooterSpeed(distance);
 
         shooterSubsystem.setRightVelocity(targetSpeed);
