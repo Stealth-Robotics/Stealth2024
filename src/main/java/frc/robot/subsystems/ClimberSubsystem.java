0a04b59e316c5dcbd31122b48d81f1693ce57022
Ian N <littlepenguin24@outlook.com>
2024-02-18 17:17:52 -0800

    First bit of code (has error)

---
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems.climbersubsystem;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.CoastOut;
+import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+public class ClimberSubsystem {
+    private final TalonFX leftCimber;
+    private final TalonFX rightClimber;
+    
+    public ClimberSubsystem(){        
+    }
+}
