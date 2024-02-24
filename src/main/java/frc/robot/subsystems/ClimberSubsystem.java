23e4539a861cd26028ed5b87b26976fa331561e7
Ian N <littlepenguin24@outlook.com>
2024-02-23 19:02:35 -0800

    Climber Subsystem Updated (should be pretty much complete)

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -8,27 +8,39 @@ import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimberSubsystem {
     private final TalonFX leftClimber;
     private final TalonFX rightClimber;
 
-    TalonFXConfiguration LEFT_TALONFX_CONFIG = new TalonFXConfiguration();
-    TalonFXConfiguration RIGHT_TALONFX_CONFIG = new TalonFXConfiguration();
+    TalonFXConfiguration CLIMBER_CONFIG = new TalonFXConfiguration();
     
     public ClimberSubsystem(){ //TODO: Get CAN IDs
-        leftClimber = new TalonFX(0);
-        rightClimber = new TalonFX(0);
+        leftClimber = new TalonFX(18);
+        rightClimber = new TalonFX(19);
         applyConfigs();
     }
 
     private void applyConfigs(){
-        LEFT_TALONFX_CONFIG.MotorOutput.NeutralMode = NeutralModeValue.Coast;
-        RIGHT_TALONFX_CONFIG = LEFT_TALONFX_CONFIG;
+        CLIMBER_CONFIG.MotorOutput.NeutralMode = NeutralModeValue.Brake;
 
-        leftClimber.getConfigurator().apply(LEFT_TALONFX_CONFIG);
-        rightClimber.getConfigurator().apply(RIGHT_TALONFX_CONFIG);
+        CLIMBER_CONFIG.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+
+        leftClimber.getConfigurator().apply(CLIMBER_CONFIG);
+        rightClimber.getConfigurator().apply(CLIMBER_CONFIG);
+
+    }
+
+    private void setPower(double power){
+        leftClimber.set(power);
+        rightClimber.set(power);
+    }
+
+    public Command setPowerCommand(double power){
+       return new InstantCommand(()-> setPower(power));
 
     }
 }
